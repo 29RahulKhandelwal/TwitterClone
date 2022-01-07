6e743ee27c8128facdc993ded522b815811f7458
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-01-07 19:45:42 +0530

    Outputting the users

--- public/js/followers&followingPage.js
+++ public/js/followers&followingPage.js
@@ -7,15 +7,39 @@ $(document).ready(()=>{
 });
 function loadFollowers(){
     $.get(`/api/users/${profileUserId}/followers`, results=>{
-        outputUsers(results,$(".resultsContainer"));
+        outputUsers(results.followers,$(".resultsContainer"));
     })
 }
 function loadFollowing(){
     $.get(`/api/users/${profileUserId}/following`, results=>{
-        outputUsers(results,$(".resultsContainer"));
+        outputUsers(results.following,$(".resultsContainer"));
     })
 }
 
-function outputUsers(data,container){
-    console.log(data)
+function outputUsers(results,container){
+    container.html("");
+
+    results.forEach(result=>{
+        var html=createUserhtml(result,true);
+        container.append(html);
+    })
+    if(results.length===0){
+        container.append("<span class='noResults'>No results found</span>")
+    }
+
+}
+
+function createUserhtml(userData,showFollowButton){
+    var name=userData.firstname +" "+ userData.lastname;
+    return `<div class="user">
+                <div class="userImageContainer">
+                    <img src="${userData.profilePic}">
+                </div>
+                <div class="userDetailsContainer">
+                    <div class="header">
+                        <a href="/profile/${userData.username}">${name}</a>
+                        <span classs="username">@${userData.username}</span>
+                    </div>
+                </div>
+            </div>`;
 }
