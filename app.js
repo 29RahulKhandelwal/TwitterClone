00aad42a518311932e789f07d772b935f30ceff3
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-01-11 18:26:01 +0530

    getting the chats

--- app.js
+++ app.js
@@ -576,6 +576,13 @@ app.post("/api/chats", async (req, res, next) => {
     })
 })
 
+app.get("/api/chats", async (req, res, next) => {
+    Chat.find({users:{$elemMatch:{$eq:req.session.user._id}}})
+    .then(results=>res.send(results))
+    .catch(error=>console.log(error))
+})
+
+
 app.get("/logout",(req,res,next)=>{
     if(req.session){
         req.session.destroy(()=>{
