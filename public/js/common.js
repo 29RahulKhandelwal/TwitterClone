95379d189a6f17b5b3e68a577cac1eeafbb0f1ee
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-01-11 15:00:52 +0530

    Leaving  Selected Users out of the search result

--- public/js/common.js
+++ public/js/common.js
@@ -495,7 +495,7 @@ function outputSelectableUsers(results,container){
     container.html("");
 
     results.forEach(result=>{
-        if(result._id == userLoggedIn._id){
+        if(result._id == userLoggedIn._id || selectedUsers.some(user=>user._id==result._id)){
             return;
         }
         var html=createUserhtml(result,true);
