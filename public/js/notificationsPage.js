df7e08fd674203925793a9fedaa6697b686613d9
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-01-24 14:26:40 +0530

    created notification HTML

--- public/js/notificationsPage.js
+++ public/js/notificationsPage.js
@@ -1,5 +1,27 @@
 $(document).ready(()=>{
     $.get("/api/notifications",(data)=>{
-        console.log(data);
+        outputNotificationList(data,$(".resultsContainer"));
     })
 })
+
+function outputNotificationList(notifications,container){
+    notifications.forEach(notification=>{
+        var html=createNotificationHtml(notification);
+        container.append(html)
+    })
+    if(notifications.length==0){
+        container.append("<span class='noResults'>Nothing to show.</span>")
+    }
+}
+
+function createNotificationHtml(notification){
+    var userFrom=notification.userFrom;
+    return `<a href="#" class="resultListItem notification">
+                <div class="resultsImageContainer">
+                    <img src="${userFrom.profilePic}"></img>
+                </div>
+                <div class="resultsDetailsContainer ellipsis">
+                    <span class="ellipsis">This is the Text</span>
+                </div>
+            </a>`
+}
