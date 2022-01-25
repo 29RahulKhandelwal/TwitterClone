8e4a82c4cddb8600ace9e63dbe6319c6d7d23b1a
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-01-25 21:20:32 +0530

    Outputting the popup notification

--- public/js/clientSocket.js
+++ public/js/clientSocket.js
@@ -4,8 +4,9 @@ const socket=io();
 socket.emit("setup",userLoggedIn);
 socket.on("connected",()=>connected=true);
 socket.on("message received",(newMessage)=>messageReceived(newMessage));
-socket.on("notification received",(newNotification)=>{
+socket.on("notification received",()=>{
     $.get("/api/notifications/latest",(notificationData)=>{
+        showNotificationPopup(notificationData);
         refreshNotificationsBadge();
     })
 })
