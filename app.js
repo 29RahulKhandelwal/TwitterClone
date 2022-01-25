cd9d896c85ec4dd130f243df1dad047694e95f0e
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-01-26 00:59:17 +0530

    addedd dotenv

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set("view engine","pug");
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
 app.use(session({
-    secret:"process.env.Secret",
+    secret:process.env.Secret,
     resave:true,
     saveUninitialized:false
 }));
